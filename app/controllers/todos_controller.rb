d4668b395df4b913d1ae686e55ab526011c3df70
MS. NATTAWADEE WUTTIVORADIT <pattynp2005@gmail.com>
2024-11-02 15:46:27 +0700

    add search panel

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,7 +3,7 @@ class TodosController < ApplicationController
 
   # GET /todos or /todos.json
   def index
-    @todos = Todo.all
+    @todos = params[:text].present? ? Todo.where("title LIKE ?", "%#{params[:text]}%") : Todo.all
   end
 
   # GET /todos/1 or /todos/1.json
